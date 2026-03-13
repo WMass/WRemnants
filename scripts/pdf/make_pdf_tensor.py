Implement average symmetrization for PDF grids, bug fixes
- Fix a sign in the unc. symmetrization for the postfit pdf grids maker
- Fix a bug where the central value was incorrectly paired with other
  variations, leading to a mismatch for the assymetric uncertainties

With these fixes the Rabbit and Grids-based plots agree very well

scripts/pdf/make_pdf_tensor.py
@@ -4,7 +4,7 @@ import hist
 import numpy as np
 
 from rabbit import inputdata, tensorwriter
-from wremnants import theory_tools
+from wremnants.utilities import theory_utils
 from wums import logging, output_tools
 
 parser = argparse.ArgumentParser()
@@ -60,20 +60,21 @@ metadata = indata.metadata
 pdf_input = indata.metadata["meta_info_input"]["args"]["pdfs"][0]
 pdf_scale = metadata["meta_info"]["args"]["scalePdf"]
 
-pdfInfo = theory_tools.pdf_info_map("Zmumu_2016PostVFP", pdf_input)
+pdfInfo = theory_utils.pdf_info_map("Zmumu_2016PostVFP", pdf_input)
 pdf_name = pdfInfo["lha_name"]
 
 if pdf_scale == -1:
-    pdf_scale = theory_tools.pdf_inflation_factor(
-        theory_tools.pdfMap[pdf_input], metadata["meta_info"]["args"]["noi"]
+    pdf_scale = theory_utils.pdf_inflation_factor(
+        theory_utils.pdfMap[pdf_input], metadata["meta_info"]["args"]["noi"]
     )
-    logger.info(f"Using default inflation factor from theory_tools: {pdf_scale}")
+    logger.info(f"Using default inflation factor: {pdf_scale}")
 
 pdf_scale *= pdfInfo["scale"] if "scale" in pdfInfo else 1.0
 logger.info(f"Scaling PDF uncertainties by {pdf_scale}")
 
 symHessian = pdfInfo["combine"] == "symHessian"
 symmetrize = indata.metadata["meta_info"]["args"]["symmetrizePdfUnc"]
+print(f"PDF symmetrization procedure: {symmetrize}")
 
 if not symHessian:
     logger.info(f"Applying {symmetrize} symmetrization procedure")
@@ -87,21 +88,24 @@ labels = np.array(
     ],
     dtype=str,
 )
-if not symHessian:
+
+if symmetrize == "quadratic":
     labels[::2] = [
         s.replace("SymAvg", "Down").replace("SymDiff", "Down") for s in labels[::2]
     ]
     labels[1::2] = [
         s.replace("SymAvg", "Up").replace("SymDiff", "Up") for s in labels[1::2]
     ]
+elif symmetrize == "average":
+    labels = [f"{l}{shift}" for l in labels for shift in ("Down", "Up")]
 
 x_range = np.logspace(-4, -0.01, 201)
 
 for chan in ["u", "ubar", "d", "dbar", "s", "sbar", "g", "uv", "dv"]:
-    pdf_data = theory_tools.get_pdf_data(pdf_name, chan, 80.360, x_range[:-1])
+    pdf_data = theory_utils.get_pdf_data(pdf_name, chan, 80.360, x_range[:-1])
     pdf_hist = hist.Hist(
         hist.axis.Variable(x_range, name="x"),
-        hist.axis.StrCategory(labels, name="pdfVar"),
+        hist.axis.StrCategory(["central", *labels], name="pdfVar", flow=False),
         data=pdf_data.T,
     )
 
@@ -123,7 +127,7 @@ for chan in ["u", "ubar", "d", "dbar", "s", "sbar", "g", "uv", "dv"]:
                 kfactor=pdf_scale,
             )
     else:
-        systs = list(pdf_hist.axes["pdfVar"])
+        systs = list(pdf_hist.axes["pdfVar"])[1:]
         for systDown, systUp in zip(systs[::2], systs[1::2]):
             writer.add_systematic(
                 [pdf_hist[..., systUp], pdf_hist[..., systDown]],

scripts/pdf/make_postfit_pdf_grids.py
@@ -12,7 +12,8 @@ from mc2hlib import lh
 from mc2hlib.common import load_pdf
 
 from rabbit import io_tools
-from wremnants import combine_helpers, theory_tools
+from wremnants.postprocessing import syst_tools
+from wremnants.utilities import theory_utils
 from wums import logging
 
 parser = argparse.ArgumentParser()
@@ -35,7 +36,7 @@ parser.add_argument(
     "--pdf-name",
     type=str,
     required=False,
-    choices=["auto", *theory_tools.pdfMap.keys()],
+    choices=["auto", *theory_utils.pdfMap.keys()],
     default="auto",
     help="Name of the PDF set to use. If 'auto', will use the PDF from the fit result metadata.",
 )
@@ -72,11 +73,6 @@ def postfit_eignvectors(cov_pdf):
     return V * np.sqrt(np.maximum(eigv, 0))
 
 
-def apply_symmetrization(matrix, symm, labels):
-    if symm == "quadratic":
-        return combine_helpers.quadratic_symmetrization(matrix, labels)
-
-
 def write_new_grids(
     base_name, outfolder, fitlabel, postfit_matrix, central_grid, pdf_scale
 ):
@@ -102,7 +98,7 @@ def write_new_grids(
         elif l.find("SetIndex:") >= 0:
             out.write(f"SetIndex: {args.lhaid}\n")
         elif l.find("NumMembers:") >= 0:
-            out.write(f"NumMembers: {nhess + 1}\n")
+            out.write(f"NumMembers: {postfit_matrix.shape[-1] + 1}\n")
         elif l.find("ErrorType") >= 0:
             out.write(f"ErrorType: symmhessian\n")
         else:
@@ -148,16 +144,16 @@ else:
             f"Specified PDF name {args.pdf_name} does not match input PDF {pdf_input}."
         )
 
-pdfInfo = theory_tools.pdf_info_map("Zmumu_2016PostVFP", pdf_input)
+pdfInfo = theory_utils.pdf_info_map("Zmumu_2016PostVFP", pdf_input)
 pdf_name = pdfInfo["lha_name"]
 pdf_scale = input_meta["meta_info"]["args"]["scalePdf"]
 pdf_symm = input_meta["meta_info"]["args"]["symmetrizePdfUnc"]
 
 if pdf_scale == -1:
-    pdf_scale = theory_tools.pdf_inflation_factor(
+    pdf_scale = theory_utils.pdf_inflation_factor(
         pdfInfo, input_meta["meta_info"]["args"]["noi"]
     )
-    logger.info(f"Using default inflation factor from theory_tools: {pdf_scale}")
+    logger.info(f"Using default inflation factor from theory_utils: {pdf_scale}")
 
 pdf_scale *= pdfInfo["scale"] if "scale" in pdfInfo else 1
 logger.info(f"Scaling PDF uncertainties by {pdf_scale}")
@@ -208,7 +204,7 @@ matrix = lh.big_matrix(grids[: nhess + 1]) * pdf_scale
 
 if not symm_errors:
     logger.info(f"Applying symmetrization {pdf_symm} to PDF uncertainties.")
-    matrix = apply_symmetrization(matrix, pdf_symm, labels)
+    matrix = syst_tools.symmetrize_unc_matrix(matrix, grids[0].values, labels, pdf_symm)
 
 new_central = grids[0] + np.sum(pulls * matrix, axis=1)
 

wremnants/postprocessing/rabbit_helpers.py
@@ -1,6 +1,5 @@
 import hist
 import numpy as np
-import pandas as pd
 
 from wremnants.postprocessing import histselections
 from wremnants.postprocessing.datagroups.datagroup import Datagroup_member
@@ -308,7 +307,7 @@ def add_electroweak_uncertainty(
             if w_samples:
                 # add renesance (virtual EW) uncertainty on W samples
                 card_tool.addSystematic(
-                    f"{ewUnc}_Corr",
+                    f"{ewUnc}Corr",
                     processes=w_samples,
                     preOp=lambda h: h[{"var": ["nlo_ew_virtual"]}],
                     labelsByAxis=[f"renesanceEWCorr"],
@@ -321,10 +320,10 @@ def add_electroweak_uncertainty(
         elif ewUnc == "powhegFOEW":
             if z_samples:
                 card_tool.addSystematic(
-                    f"{ewUnc}_Corr",
+                    f"{ewUnc}Corr",
                     preOp=lambda h: h[{"weak": ["weak_ps", "weak_aem"]}],
                     processes=z_samples,
-                    labelsByAxis=[f"{ewUnc}_Corr"],
+                    labelsByAxis=[f"{ewUnc}Corr"],
                     scale=1.0,
                     systAxes=["weak"],
                     mirror=True,
@@ -333,10 +332,10 @@ def add_electroweak_uncertainty(
                     name="ewScheme",
                 )
                 card_tool.addSystematic(
-                    f"{ewUnc}_Corr",
+                    f"{ewUnc}Corr",
                     preOp=lambda h: h[{"weak": ["weak_default"]}],
                     processes=z_samples,
-                    labelsByAxis=[f"{ewUnc}_Corr"],
+                    labelsByAxis=[f"{ewUnc}Corr"],
                     scale=1.0,
                     systAxes=["weak"],
                     mirror=True,
@@ -378,12 +377,12 @@ def add_electroweak_uncertainty(
                 preOp = lambda h: h[{"systIdx": s[1:2]}]
 
             card_tool.addSystematic(
-                f"{ewUnc}_Corr",
+                f"{ewUnc}Corr",
                 systAxes=["systIdx"],
                 mirror=True,
                 passToFakes=passSystToFakes,
                 processes=samples,
-                labelsByAxis=[f"{ewUnc}_Corr"],
+                labelsByAxis=[f"{ewUnc}Corr"],
                 scale=scale,
                 preOp=preOp,
                 groups=[f"theory_ew_{ewUnc}", "theory_ew", "theory"],
@@ -628,34 +627,6 @@ def add_bsm_process(
     # scale BSM cross section to SM cross section
     for m in datagroups.groups[bsm_name].members:
         m.xsec = xsec
-
-
-# TODO: Integrate with rabbit to avoid code duplication
-def quadratic_symmetrization(matrix, labels):
-    if type(matrix) == pd.DataFrame:
-        values = matrix.values
-    elif type(matrix) == hist.Hist:
-        values = matrix.values()
-    else:
-        values = matrix
-
-    symm_avg = 0.5 * (matrix[:, ::2] + matrix[:, 1::2])
-    symm_diff = 0.5 * np.sqrt(3) * (matrix[:, ::2] - matrix[:, 1::2])
-    avg_idx = np.char.find(labels, "Avg") != -1
-
-    if np.count_nonzero(avg_idx) != symm_avg.shape[1]:
-        raise ValueError(
-            f"Found inconsistent number of Avg nuisances (Avg: {np.count_nonzero(avg_idx)}, Symm: {symm_avg.shape[1]}, Diff {symm_diff.shape[1]}) for quadratic symmetrization."
-        )
-
-    if type(matrix) == pd.DataFrame:
-        matrix.iloc[:, avg_idx] = symm_avg
-        matrix.iloc[:, ~avg_idx] = symm_diff
-    else:
-        matrix[:, avg_idx] = symm_avg
-        matrix[:, ~avg_idx] = symm_diff
-
-    return matrix
 
 
 def decorrelateByAxis(